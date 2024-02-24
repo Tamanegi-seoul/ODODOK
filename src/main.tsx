00f5eebfd7474aa4dec5577c9ded9db227bc5051
taeyeess <taeyeeya@gmail.com>
2024-02-24 20:58:21 +0900

    style: scs파일 셋팅, 절대경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './app.scss';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
